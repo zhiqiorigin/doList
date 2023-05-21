0bf5fba21987c6ef5463ff34fa8446498ee10cb4
JinZhiqi <3099556708@qq.com>
2023-05-22 00:09:53 +0800

    数据库基本配置

---
+++ service/db/db.js
@@ -0,0 +1,24 @@
+
+const mongoose = require('mongoose')
+
+const url = 'mongodb://localhost:27017'
+const dbName = 'doList'
+
+// mongoose.set('useCreateIndex', true)
+// mongoose.set('useFindAndModify', false)
+
+// 连接 mongodb 并进入指定 database
+mongoose.connect(url + '/' + dbName, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+})
+
+// 获取连接对象，添加一个错误处理函数
+const conn = mongoose.connection
+conn.on('error', err => {
+  console.error("connection error:", err)
+})
+
+module.exports = mongoose
--- service/routes/users.js
+++ service/routes/users.js
@@ -2,12 +2,59 @@ const router = require('koa-router')()
 
 router.prefix('/users')
 
-router.get('/', function (ctx, next) {
-  ctx.body = 'this is a users response!'
+import {login,register} from '../controller/user'
+// 获取用户信息
+router.get('/getUserInfo', loginCheck, async (ctx, next) => {
+  ctx.body = {
+    errno: 0,
+    data: ctx.session.userInfo
+  }
+})
+// 登录
+router.post('/login', async (ctx, next) => {
+  // 获取登录信息
+  const { username, password } = ctx.request.body
+  // 验证登录
+  const res = await login(username, password)
+  if (res) {
+    // 登录成功
+
+    // 设置 session
+    ctx.session.userInfo = {
+      username
+    }
+
+    // 返回信息
+    ctx.body = {
+      errno: 0
+    }
+  } else {
+    // 登陆失败
+    ctx.body = {
+      errno: -1,
+      message: '登录验证失败'
+    }
+  }
 })
 
-router.get('/bar', function (ctx, next) {
-  ctx.body = 'this is a users/bar response'
+// 注册
+router.post('/register', async (ctx, next) => {
+  // 获取注册信息
+  const userInfo = ctx.request.body
+  // 提交注册
+  try {
+    const newUser = await register(userInfo)
+    ctx.body = {
+      errno: 0,
+      data: newUser
+    }
+  } catch (err) {
+    console.error('注册失败', err)
+    ctx.body = {
+      errno: -1,
+      message: "注册失败"
+    }
+  }
+  
 })
-
 module.exports = router
